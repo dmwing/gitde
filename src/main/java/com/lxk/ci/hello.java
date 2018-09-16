261111223bd8339b416f48f19d20286aa8165867
dmwing <60585776@qq.com>
2018-09-16 16:19:47 +0800

    v2

--- src/main/java/com/lxk/ci/hello.java
+++ src/main/java/com/lxk/ci/hello.java
@@ -1,7 +1,7 @@
 package com.lxk.ci;
 
 public class hello {
-    private String appName = "Hello11";
+    private String appName = "Hello2";
     public String getAppName() {
         return appName;
     }
