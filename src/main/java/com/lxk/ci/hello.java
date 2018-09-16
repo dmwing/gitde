968ebcfcf2ceb553dabfad09cd9ff08bc0cdfa11
dmwing <60585776@qq.com>
2018-09-16 16:39:50 +0800

    提交测试

--- src/main/java/com/lxk/ci/hello.java
+++ src/main/java/com/lxk/ci/hello.java
@@ -1,7 +1,7 @@
 package com.lxk.ci;
 
 public class hello {
-    private String appName = "Hello11";
+    private String appName = "Hello111";
     public String getAppName() {
         return appName;
     }
