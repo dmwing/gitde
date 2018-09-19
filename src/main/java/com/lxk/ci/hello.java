19bab32f77b0c91f61912e68235f8bec16a00873
dmwing <60585776@qq.com>
2018-09-19 20:54:51 +0800

    sdsd

--- src/main/java/com/lxk/ci/hello.java
+++ src/main/java/com/lxk/ci/hello.java
@@ -1,7 +1,7 @@
 package com.lxk.ci;
 
 public class hello {
-    private String appName = "Hello v2 v1";
+    private String appName = "Hello v1 v2";
     public String getAppName() {
         return appName;
     }
